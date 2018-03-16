62f623f82c9be01a558a5b2b0cb070263504935b
leontin26 <leontin26@yahoo.com>
2018-03-16 18:59:05 +0200

    orientation5

--- x3d/orientare.js
+++ x3d/orientare.js
@@ -4,5 +4,5 @@ function on_device_orientation(e)
 {
 	document.getElementById("id_orientation").innerHTML = e.alpha.toFixed(1) + " " + e.beta.toFixed(1)+ " " + e.gamma.toFixed(1);
 	var viewpoint = document.getElementById("id_viewpoint");
-	viewpoint.setAttribute("orientation", "0 0  1 " + e.alpha * Math.Pi / 180);
+	viewpoint.setAttribute("orientation", "0 0  1 " + e.alpha * Math.PI / 180);
 }
